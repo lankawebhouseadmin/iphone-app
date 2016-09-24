ade112533c7c3ec7f0a933675a401d0f4eb2ebda
Arik Volodin <arikvolodin0415@gmail.com>
2016-09-24 16:14:48 -0700

    first commit

---
+++ Be24there/Be24there/Classes/Model/BE24Model.swift
@@ -0,0 +1,13 @@
+//
+//  BE24Model.swift
+//  Be24there
+//
+//  Created by Arik on 9/23/16.
+//  Copyright © 2016 Prbath. All rights reserved.
+//
+
+import UIKit
+
+class BE24Model: NSObject {
+
+}
