71818f9dea6e41a358f73c01562c821c158ba9e9
Arik Volodin <arikvolodin0415@gmail.com>
2016-09-25 00:03:05 -0700

    Change developer and copyright name

---
+++ Bethere24/Bethere24/Classes/Controller/BE24NavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  BE24NavigationController.swift
+//  Bethere24
+//
+//  Created by Prbath Neranja on 9/24/16.
+//  Copyright © 2016 LankaWebHouse. All rights reserved.
+//
+
+import UIKit
+
+class BE24NavigationController: UINavigationController {
+
+}
---
+++ Bethere24/Bethere24/Classes/Controller/BE24TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BE24TableViewController.swift
+//  Bethere24
+//
+//  Created by Prbath Neranja on 9/24/16.
+//  Copyright © 2016 LankaWebHouse. All rights reserved.
+//
+
+import Cocoa
+
+class BE24TableViewController: UITableViewController {
+
+}
